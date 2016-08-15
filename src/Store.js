4137480fc1e08a66e08e378d62a00cb40d652628
justsayno <contact@sethreid.co.nz>
2016-08-15 20:47:44 +1200

    added provider

--- src/Store.js
+++ src/Store.js
@@ -2,7 +2,7 @@ import { createStore } from 'redux'
 import { getEmployees } from './api/employees'
 
 const initialState = {
-    employees = getEmployees()
+    employees: getEmployees()
 }
 
 const employeeReducer = (state = initialState, action) => {
