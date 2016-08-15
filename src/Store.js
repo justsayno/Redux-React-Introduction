b29eaec72202722a165e7f61f3563f73dff47e04
justsayno <contact@sethreid.co.nz>
2016-08-15 21:00:16 +1200

    added first action

--- src/Store.js
+++ src/Store.js
@@ -1,6 +1,15 @@
 import { createStore } from 'redux'
 import { getEmployees } from './api/employees'
 
+// Actions
+const REQUEST_EMPLOYEE = 'REQUEST_EMPLOYEE'
+
+// Action Creators
+export const requestEmployee = (id) => ({
+    type: REQUEST_EMPLOYEE,
+    id: id
+})
+
 const initialState = {
     employees: getEmployees()
 }
