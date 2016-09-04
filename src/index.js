9d1121ce7b26547ca7a7b99d79cd278212339377
Seth Reid <contact@sethreid.co.nz>
2016-09-04 14:21:15 +1200

    fixed store

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom'
 import Routes from './routes'
 
 import { Provider } from 'react-redux'
-import { store } from './Store'
+import { store } from './store'
 
 ReactDOM.render(
   <Provider store={store}>
