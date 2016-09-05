14a2d6381a7a132ea3607b1f63f289712a1a6288
Seth Reid <contact@sethreid.co.nz>
2016-09-05 18:45:29 +1200

    updated

--- src/components/Error.js
+++ src/components/Error.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
 const Error = ({error}) => (
-    <div>
+    <div className='error center-align'>
         <p>Sorry something went wrong...</p>
-        <p>Message:</p>
+        <img src='/src/images/error.png' />
         <p>{error}</p>
     </div>
 )
